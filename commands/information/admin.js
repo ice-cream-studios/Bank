a45b44b070a3261bb13ee029097f6d0c2443f6d8
Camila R <camila@finley.codes>
2022-08-18 16:51:00 +0100

    Migration to Discord.js v14

--- commands/information/admin.js
+++ commands/information/admin.js
@@ -1,9 +1,9 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     slashData: new SlashCommandBuilder()
         .setName("admin")
         .setDescription("Do you have admin perms?"),
-    permissions: ['ADMINISTRATOR'],
+    permissions: ['Administrator'],
     run: async function(client, interaction) {
         interaction.reply(`it works, you have admin`);
     }
